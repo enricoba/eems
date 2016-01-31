f4bcbb5acb90da0441d7e86258e2901d38fae6aa
enricoba <henrik.baran@online.de>
2016-01-31 13:28:24 +0100

    changed version to 0.0.4.0a1 and status alpha

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eems',
-    version='0.0.3.dev1',
+    version='0.0.4.0a1',
     description='An easy application to establish an energy monitoring system.',
     long_description='test',
     url='https://github.com/enricoba/eems',
@@ -21,6 +21,11 @@ setup(
     ],
     keywords='easy energy monitoring system',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    package_data={
+        'eems': [
+            'data/*.txt'
+        ]
+    },
     entry_points={
         'console_scripts': [
             'eems = eems.scripts:main'
