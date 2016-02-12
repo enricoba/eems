c4f657f257dfe16e222c9fb44e6fa176917a4e29
enricoba <henrik.baran@online.de>
2016-02-12 16:24:46 +0100

    some tests with numpy

--- eems/analysis.py
+++ eems/analysis.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
+from numpy import genfromtxt
+
+
 def analysis():
     print 'statistische auswertung'
 
 
-def import_data():
-    if True:
-        'print test'
+def import_data(csv_file):
+    content = genfromtxt(csv_file, delimiter=";", skip_header=1, dtype=None,
+                         unpack=True)
+    return content
+
