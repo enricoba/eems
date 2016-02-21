85a9a995805982846b6856b65b48bdcc6ba50df0
enricoba <henrik.baran@online.de>
2016-02-21 21:08:42 +0100

    test for reports

--- eems/analysis.py
+++ eems/analysis.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
+import time
 import numpy as np
 
 
@@ -8,8 +9,12 @@ def analysis(content):
     return np.mean(content)
 
 
-def import_data(csv_file):
-    if isinstance(csv_file, basestring) is True:
-        content = np.genfromtxt(csv_file, delimiter=';', skip_header=True)
-        return content
-
+def import_data(csv_file, sensor_count):
+    with open(csv_file, 'rb') as csv:
+        header = csv.readlines()[0].split(';')
+    data_types_base = [int, time.struct_time, time.struct_time]
+    data_types_sensors = sensor_count * [float]
+    data_types = data_types_base + data_types_sensors
+    content = np.genfromtxt(csv_file, delimiter=';', names=header,
+                            skip_header=True, dtype=data_types)
+    return content
