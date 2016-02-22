927ab0cb9f477419c2df8983ff61ccf170b68cab
enricoba <henrik.baran@online.de>
2016-02-22 21:28:21 +0100

    loaded config file .ini with ConfigParser

--- eems/__init__.py
+++ eems/__init__.py
@@ -1,4 +1,6 @@
 from ds18b20 import Temp, Check
+import ConfigParser
+import ast
 
 
 __project__ = 'eems'
@@ -6,5 +8,11 @@ __version__ = '0.1.0.5.b1'
 __copyright__ = '2016, Henrik Baran, Aurofree Hoehn'
 __author__ = 'Henrik Baran, Aurofree Hoehn'
 
+
+config = ConfigParser.ConfigParser()
+config.read('data/eems.ini')  # TODO muss noch in package data geladen werden
+print config.sections()
+log = ast.literal_eval(config.get('log', 'log'))
+
+
 # TODO define central logger
-# TODO add config file
