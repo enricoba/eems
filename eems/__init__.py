8346a93224abfdace272cf583f57076f145ee31b
enricoba <henrik.baran@online.de>
2016-03-05 22:09:30 +0100

    new init

--- eems/__init__.py
+++ eems/__init__.py
@@ -3,9 +3,8 @@
 Initiation module for eems.
 """
 
-from sensors.ds18b20 import DS18B20
-from main.init import init
-# import logging
+# from sensors.ds18b20 import DS18B20
+# from main.init import init
 
 
 """
@@ -13,7 +12,7 @@ eems project information
 """
 
 
-__all__ = ['init', 'DS18B20']
+__all__ = ['main', 'sensors']
 __project__ = 'eems'
 __version__ = '0.1.0.5.b1'
 __copyright__ = '2016, Henrik Baran, Aurofree Hoehn'
