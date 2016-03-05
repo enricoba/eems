12bffa1e17c266ba36c779a3d3a79b336ad5c804
enricoba <henrik.baran@online.de>
2016-03-05 21:55:24 +0100

    new init

--- eems/__init__.py
+++ eems/__init__.py
@@ -5,7 +5,7 @@ Initiation module for eems.
 
 from sensors.ds18b20 import DS18B20
 from main.init import init
-# import logging
+import logging
 # from eems.support.handlers import StatusHandler
 
 
@@ -14,7 +14,7 @@ eems project information
 """
 
 
-__all__ = ['init', 'DS18B20']
+__all__ = ['logging', 'init', 'DS18B20']
 # __all__ = ['sensors', 'main', 'support']
 __project__ = 'eems'
 __version__ = '0.1.0.5.b1'
