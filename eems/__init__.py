df33d2cc1d4f36dabbffc205996b0e0119043e46
enricoba <henrik.baran@online.de>
2016-03-05 21:53:22 +0100

    new init

--- eems/__init__.py
+++ eems/__init__.py
@@ -3,16 +3,21 @@
 Initiation module for eems.
 """
 
-from eems.support.handlers import StatusHandler
+from sensors.ds18b20 import DS18B20
+from main.init import init
+# import logging
+# from eems.support.handlers import StatusHandler
+
 
 """
 eems project information
 """
 
 
-__all__ = ['sensors', 'main', 'support']
+__all__ = ['init', 'DS18B20']
+# __all__ = ['sensors', 'main', 'support']
 __project__ = 'eems'
 __version__ = '0.1.0.5.b1'
 __copyright__ = '2016, Henrik Baran, Aurofree Hoehn'
 __author__ = 'Henrik Baran, Aurofree Hoehn'
-__init__ = StatusHandler(False)
+# __init__ = StatusHandler(False)
