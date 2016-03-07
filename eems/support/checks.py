00acc3d53df58efb5fc9a026a7f9b37d5cefceb5
enricoba <henrik.baran@online.de>
2016-03-07 18:30:59 +0100

    super duper pew pew

--- eems/support/checks.py
+++ eems/support/checks.py
@@ -6,14 +6,6 @@ Checks module provides functions to check sensor requirements.
 import logging
 
 
-"""
-defining logger
-"""
-
-logger = logging.getLogger(__name__)
-print 'load check'
-
-
 class Check(object):
     def __init__(self):
         """Public class *Check* provides functions to validate system
@@ -23,7 +15,7 @@ class Check(object):
             Returns an object providing the public functions *w1_config* and
             *w1_modules*.
         """
-        print 'init von check'
+        self.logger = logging.getLogger(__name__)
         self.dir_modules = '/etc/modules'
         self.dir_config = '/boot/config.txt'
         self.flag = {'w1-therm': False,
@@ -49,23 +41,23 @@ class Check(object):
             Returns *True* if check passed. Otherwise *False*.
         """
         if quiet is True:
-            logger.disabled = True
+            self.logger.disabled = True
         try:
             with open(self.dir_config, 'r') as config_file:
                 config = config_file.readlines()
         except IOError as e:
-            logger.error('{}'.format(e))
+            self.logger.error('{}'.format(e))
         else:
             check = [c for c in config if c.strip('\n')[:17] ==
                      'dtoverlay=w1-gpio']
             if len(check) == 0:
-                logger.error('Config.txt check failed: "dtoverlay=w1-gpio"'
+                self.logger.error('Config.txt check failed: "dtoverlay=w1-gpio"'
                              ' is not set')
-                logger.info('Please use the command script <sudo eems '
+                self.logger.info('Please use the command script <sudo eems '
                             'prepare> to prepare "/boot/config.txt"')
                 return False
             else:
-                logger.info('Config.txt check ok: "dtoverlay=w1-gpio" is set')
+                self.logger.info('Config.txt check ok: "dtoverlay=w1-gpio" is set')
                 return True
 
     def w1_modules(self):
@@ -88,29 +80,29 @@ class Check(object):
             Returns *True* if check passed. Otherwise returns *False*.
         """
         if quiet is True:
-            logger.disabled = True
+            self.logger.disabled = True
         try:
             with open(self.dir_modules, 'r') as modules_file:
                 modules = modules_file.readlines()
         except IOError as e:
-            logger.error('{}'.format(e))
+            self.logger.error('{}'.format(e))
         else:
             check_therm = [c for c in modules if c.strip('\n') == 'w1-therm']
             check_gpio = [c for c in modules if c.strip('\n') == 'w1-gpio']
             if len(check_therm) == 1:
-                logger.info('Module check ok: "w1-therm" is set')
+                self.logger.info('Module check ok: "w1-therm" is set')
                 self.flag['w1-therm'] = True
             else:
-                logger.error('Module check failed: "w1-therm" is not set')
+                self.logger.error('Module check failed: "w1-therm" is not set')
             if len(check_gpio) == 1:
-                logger.info('Module check ok: "w1-gpio" is set')
+                self.logger.info('Module check ok: "w1-gpio" is set')
                 self.flag['w1-gpio'] = True
             else:
-                logger.error('Module check failed: "w1-gpio" is not set')
+                self.logger.error('Module check failed: "w1-gpio" is not set')
             if self.flag['w1-therm'] is True and self.flag['w1-gpio'] is True:
                 return True
             else:
-                logger.info('Please use the command script <sudo eems '
+                self.logger.info('Please use the command script <sudo eems '
                             'prepare> to prepare "/etc/modules"')
                 return False
 
@@ -123,19 +115,19 @@ class Check(object):
             Returns *None*.
         """
         if self.__w1_config(quiet=True) is False:
-            logger.disabled = False
+            self.logger.disabled = False
             try:
                 with open(self.dir_config, 'a') as config_file:
                     config_file.write('dtoverlay=w1-gpio\n')
             except IOError as e:
-                logger.error('{}'.format(e))
+                self.logger.error('{}'.format(e))
             else:
-                logger.info('Config.txt has been prepared successfully')
+                self.logger.info('Config.txt has been prepared successfully')
         else:
-            logger.disabled = False
+            self.logger.disabled = False
 
         if self.__w1_modules(quiet=True) is False:
-            logger.disabled = False
+            self.logger.disabled = False
             try:
                 if self.flag['w1-therm'] is False:
                     with open(self.dir_modules, 'a') as modules_file:
@@ -144,8 +136,8 @@ class Check(object):
                     with open(self.dir_modules, 'a') as modules_file:
                         modules_file.write('w1-gpio\n')
             except IOError as e:
-                logger.error('{}'.format(e))
+                self.logger.error('{}'.format(e))
             else:
-                logger.info('Modules have been prepared successfully')
+                self.logger.info('Modules have been prepared successfully')
         else:
-            logger.disabled = False
+            self.logger.disabled = False
