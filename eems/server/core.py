baa5e58e85ce9d9650efb7e3103eb4ed13ff2769
enricoba <henrik.baran@online.de>
2016-04-04 14:17:28 +0200

    server starts

---
+++ eems/server/core.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Server core module
+"""
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run()
