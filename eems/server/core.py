c47f30db5214d743bac02c5ae2286607456d8576
enricoba <henrik.baran@online.de>
2016-04-12 23:46:38 +0200

    homepage session

--- eems/server/core.py
+++ eems/server/core.py
@@ -10,10 +10,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-@app.route('/<name>')
-def homepage(name=None):
-    return render_template("index.html", name=name)
+@app.route("/eems/")
+# @app.route('/eems/<config>/')
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
