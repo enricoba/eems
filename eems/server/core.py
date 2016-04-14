d4ed3882eaac8a66ef92024e475eb7a6e2dcdf71
enricoba <henrik.baran@online.de>
2016-04-14 17:24:39 +0200

    variable issue fixed ...

--- eems/server/core.py
+++ eems/server/core.py
@@ -5,31 +5,30 @@ Server core module
 
 
 from flask import Flask, render_template
-# from eems import __version__
+from eems import __version__
 
 
 app = Flask(__name__)
 
 
-
 @app.route("/eems/")
 def index():
-    return render_template("index.html", name='index')
+    return render_template("index.html", name='index', version=__version__)
 
 
 @app.route("/eems/config/")
 def config():
-    return render_template("index.html", name='config')
+    return render_template("index.html", name='config', version=__version__)
 
 
 @app.route("/eems/monitor/")
 def monitor():
-    return render_template("index.html", name='monitor')
+    return render_template("index.html", name='monitor', version=__version__)
 
 
 @app.route("/eems/licence/")
 def licence():
-    return render_template("index.html", name='licence')
+    return render_template("index.html", name='licence', version=__version__)
 
 
 if __name__ == "__main__":
