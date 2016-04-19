b926d8eba5042e133864b96f32eab1a3dee14b02
enricoba <henrik.baran@online.de>
2016-04-19 16:17:00 +0200

    some fixes

--- eems/server/core.py
+++ eems/server/core.py
@@ -5,12 +5,7 @@ Server core module
 
 
 from flask import Flask, render_template, request
-# from OpenSSL import SSL
 from eems import __version__
-
-
-# context = SSL.Context(SSL.TLSv1_1_METHOD)
-# context.set_options(SSL.OP_NO_SSLv2)
 
 
 app = Flask(__name__)
