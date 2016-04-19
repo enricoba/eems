0325bb6d8fee090488b5f5557dac643046621793
enricoba <henrik.baran@online.de>
2016-04-19 13:46:17 +0200

    POST method and some icons

--- eems/server/core.py
+++ eems/server/core.py
@@ -4,16 +4,13 @@ Server core module
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # from OpenSSL import SSL
 from eems import __version__
 
 
 # context = SSL.Context(SSL.TLSv1_1_METHOD)
 # context.set_options(SSL.OP_NO_SSLv2)
-
-# variables
-flag = False
 
 
 app = Flask(__name__)
@@ -24,9 +21,25 @@ def index():
     return render_template("index.html", name='index', version=__version__)
 
 
-@app.route("/eems/config/")
+@app.route("/eems/config/", methods=['GET', 'POST'])
 def config():
-    return render_template("index.html", name='config', version=__version__)
+    if request.method == 'POST':
+        c_1 = 'c_1' in request.form
+        c_2 = 'c_2' in request.form
+        print 'c_1: ', c_1
+        print 'c_2: ', c_2
+        if c_1 is True and c_2 is True:
+            success = 'display: true'
+            print 'success'
+        else:
+            success = 'display: none'
+            print 'no success'
+        print 'POST  method'
+    else:
+        success = 'display: none'
+        print 'GET / else method'
+    return render_template("index.html", name='config', version=__version__,
+                           success=success)
 
 
 @app.route("/eems/monitor/")
