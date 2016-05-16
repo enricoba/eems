b6cc8f6e43fb698f78dad21748b467c911277159
Henrik Baran <henrik.baran@online.de>
2016-05-16 12:05:10 +0800

    improved workflow
    identify user name to manage folder implementation and permission management

--- eems/support/shell.py
+++ eems/support/shell.py
@@ -5,10 +5,9 @@ This module shell includes functions interacting with the shell.
 
 
 import subprocess
-import getpass
 
 
-def set_permissions():
+def set_permissions(user):
     """Public function *set_permissions* sets the permissions of the directory
         */home/$USER/eems* to *$USER:$USER*. *$USER* will be replaced with the actual user's name.
         Requires *sudo*!
@@ -16,6 +15,5 @@ def set_permissions():
     :return:
         Returns *None*.
     """
-    user = getpass.getuser()
     args = ['sudo', 'chown', '-cR', '{}:{}'.format(user, user), '/home/{}/eems'.format(user)]
     subprocess.Popen(args)
--- setup.py
+++ setup.py
@@ -48,4 +48,4 @@ setup(
 )
 
 # set correct rights
-set_permissions()
+set_permissions(user)
