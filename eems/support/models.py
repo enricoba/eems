8f3a6eec2dad9e33183a17ef3320d45cf90f7258
Henrik Baran <henrik.baran@online.de>
2016-06-18 10:11:11 +0200

    update fixed

--- eems/support/models.py
+++ eems/support/models.py
@@ -4,10 +4,12 @@ from database import Base
 
 class General(Base):
     __tablename__ = 'GENERAL'
-    item = Column('ITEM', Text, primary_key=True, unique=True)
+    id = Column('ID', Integer, primary_key=True, unique=True)
+    item = Column('ITEM', Text)
     value = Column('VALUE', Text)
 
-    def __init__(self, item=None, value=None):
+    def __init__(self, id=None, item=None, value=None):
+        self.id = id
         self.item = item
         self.value = value
 
@@ -19,7 +21,8 @@ class Content(Base):
     german = Column('GERMAN', Text)
     english = Column('ENGLISH', Text)
 
-    def __init__(self, position=None, german=None, english=None):
+    def __init__(self, id=None, position=None, german=None, english=None):
+        self.id = id
         self.position = position
         self.german = german
         self.english = english
@@ -32,7 +35,8 @@ class Sessions(Base):
     frontend = Column('FRONTEND', Text)
     backend = Column('BACKEND', Text)
 
-    def __init__(self, session=None, frontend=None, backend=None):
+    def __init__(self, id=None, session=None, frontend=None, backend=None):
+        self.id = id
         self.session = session
         self.frontend = frontend
         self.backend = backend
