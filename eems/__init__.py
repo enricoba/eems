e3bffaefaa915d12b204cb3c57bc5ed094ee9b80
Henrik Baran <henrik.baran@online.de>
2016-07-24 11:36:27 +0200

    matching

--- eems/__init__.py
+++ eems/__init__.py
@@ -539,5 +539,19 @@ def licence(lang=None):
 
 if __name__ == "__main__":
     # in deployment MUST be False !!!
-    app.debug = True
+    import logging
+
+    logFormatStr = '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
+    logging.basicConfig(format=logFormatStr, filename="global.log", level=logging.DEBUG)
+    formatter = logging.Formatter(logFormatStr, '%m-%d %H:%M:%S')
+    fileHandler = logging.FileHandler("summary.log")
+    fileHandler.setLevel(logging.DEBUG)
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setLevel(logging.DEBUG)
+    streamHandler.setFormatter(formatter)
+    app.logger.addHandler(fileHandler)
+    app.logger.addHandler(streamHandler)
+    app.logger.info("Logging is set up.")
+    # app.debug = True
     app.run(host='0.0.0.0')
